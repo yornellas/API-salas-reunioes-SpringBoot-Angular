8ca844f12167477cab5e33d87fc5b48db3a1a5f5
Yolanda Ornellas <ymf.ornellas@icloud.com>
2021-08-25 09:33:48 -0300

    first commit - project structure and H2 configuration

---
+++ src/main/java/com/github/yornellas/meetingmanager/MeetingManagerApplication.java
@@ -0,0 +1,13 @@
+package com.github.yornellas.meetingmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetingManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetingManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/yornellas/meetingmanager/MeetingManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.yornellas.meetingmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetingManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
